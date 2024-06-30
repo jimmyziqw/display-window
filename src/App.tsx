09787ffc588e34c7bb31a20ed056aa1c5cf19593
jimmyziqw <jimmywang1300@gmail.com>
2024-06-29 20:22:49 -0400

    refactor select

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,14 @@ import useStats from "./utils/useStats.ts";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 // import CustomControl from "./utils/CustomControl.tsx";
 import Scene from "./Scene.tsx";
-// import DirectionalLight from "./components/DirectionalLight.tsx";
-// import AmbientLight from "./components/AmbientLight.tsx";
 import Loader from "./utils/Loader.tsx";
+import DirectionalLight from "./components/DirectionalLight.tsx";
+
 useGLTF.preload("models/model.glb");
 
 export default function App() {
     return (
         <>
-            {/* <App2/> */}
             <Canvas
                 frameloop="always"
                 shadows 
@@ -24,14 +23,15 @@ export default function App() {
                 }}
             >
                 <OrbitControls />
+                <DirectionalLight name="primary" />
+                <DirectionalLight name="secondary" />
+                <ambientLight intensity={0.5} />
                 <Suspense fallback={null}>
                     <Preload all />
                     <Scene />
-                    
                 </Suspense>
                 {process.env.NODE_ENV === "development" && <axesHelper position={[0, 0, 0]} />}
             </Canvas>
-            {/* <Leva/> */}
             {/* <Loader setProgress={setProgress} /> */}
         </>
     );
--- src/Scene.tsx
+++ src/Scene.tsx
@@ -4,30 +4,56 @@ import { EffectComposer, Outline, Select, Selection } from "@react-three/postpro
 import * as THREE from "three";
 import Background from "./components/Background.tsx";
 import useRespondAspectChange from "./utils/useRespondAspectChange.ts";
-import DirectionalLight from "./components/DirectionalLight.tsx";
 import useNode from "./hooks/useNode.ts";
 
 export default function Scene() {
     useRespondAspectChange();
-    // const { nodes } = useGLTF("models/model.glb");
+    const [selected, setSelected] = useState<string | null>(null);
+    const [hovered, setHovered] = useState<string | null>(null);
+
+    const items = ["window", "window001", "desk", "lamp"].map((name) => (
+        <Select
+            enabled={selected === name || hovered === name}
+            onPointerOver={() => setHovered(name)}
+            onPointerOut={() => setHovered(null)}
+            onClick={() => setSelected(name)}
+        >
+            <StandardMesh name={name} />
+        </Select>
+    ));
     return (
         <>
-            <group>
-                <DirectionalLight name="primary" />
-                <DirectionalLight name="secondary" />
-                <ambientLight intensity={0.5} />
-                <Selection>
-                    <StandardMesh name="window" />
-                    <StandardMesh name="desk" />
-                    <StandardMesh name="lamp" />
-                    <Effects />
-                </Selection>
-                <StandardMesh name="window001" />
-                <StandardMesh name="floor" />
-                <Background name="background" texturePath="textures/solarium5.jpg" />
-                <Background name="background001" texturePath="textures/solarium5.jpg" />
-            </group>
+			<Selection>
+				{items}
+				<Effects selected={selected} hovered={hovered} />
+			</Selection>
+			<StandardMesh name="window001" />
+			<StandardMesh name="floor" />
+			<Background name="background" texturePath="textures/solarium5.jpg" />
+			<Background name="background001" texturePath="textures/solarium5.jpg" />
         </>
+    );
+}
+
+
+function Effects({ selected, hovered }: { selected: string | null; hovered: string | null }) {
+    const selectedObjects = selected ? [selected] : [];
+    const hoveredObjects = hovered && hovered !== selected ? [hovered] : selectedObjects;
+    return (
+        <EffectComposer multisampling={0} autoClear={false}>
+            <Outline
+                blur
+                edgeStrength={10}
+                width={1000}
+                visibleEdgeColor={
+                    hoveredObjects.length > 0
+                        ? hoveredObjects[0] === selected
+                            ? 0xffa500
+                            : 0xffffff
+                        : 0xffffff
+                }
+            />
+        </EffectComposer>
     );
 }
 
@@ -37,21 +63,6 @@ type StandardMeshProps = {
     isVisible?: boolean;
     onClick?: () => void;
 };
-
-function Effects() {
-    return (
-        <EffectComposer multisampling={2} autoClear={false}>
-            <Outline
-                blur
-                visibleEdgeColor={0xffffff}
-                edgeStrength={10}
-                width={1000}
-                selectionLayer={10}
-            />
-        </EffectComposer>
-    );
-}
-
 export const StandardMesh = ({
     name,
     texture = null,
@@ -61,7 +72,6 @@ export const StandardMesh = ({
 }: StandardMeshProps) => {
     const mesh = useNode(name);
     const meshRef = useRef(mesh);
-    const [hovered, hover] = useState(false);
 
     const material = useMemo(() => {
         if (texture) {
@@ -81,23 +91,16 @@ export const StandardMesh = ({
     }, [texture]);
 
     return (
-        <Select
-            enabled={hovered}
-            onPointerOver={() => hover(true)}
-            onPointerOut={() => hover(false)}
-        >
-            <mesh
-                castShadow
-                receiveShadow
-                ref={meshRef}
-                geometry={mesh.geometry}
-                material={texture ? material : mesh.material}
-                position={mesh.position}
-                rotation={mesh.rotation}
-                name={mesh.name}
-                {...props}
-                // onClick={onClick}
-            />
-        </Select>
+        <mesh
+            castShadow
+            receiveShadow
+            ref={meshRef}
+            geometry={mesh.geometry}
+            material={texture ? material : mesh.material}
+            position={mesh.position}
+            rotation={mesh.rotation}
+            name={mesh.name}
+            {...props}
+        />
     );
 };
