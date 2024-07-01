10ba9809b1a59be5f59e86ba9840612e20b657e3
jimmyziqw <jimmywang1300@gmail.com>
2024-07-01 17:09:49 -0400

    add netlify redirects

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/window-display/', // Set the correct base path for your project
+
 })
+
