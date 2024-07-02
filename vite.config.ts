f0a4155bfe4f9e11cca5d0abf4bd017c9b950002
jimmyziqw <jimmywang1300@gmail.com>
2024-07-02 14:24:18 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/window-display/", 
   plugins: [react()],
+  build: {
+    outDir: 'dist'  // Ensure this matches the publish_dir in the workflow
+  }
 
 })
 
