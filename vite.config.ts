23681dc9d21207fabd870275aa49944db859ae67
jimmyziqw <jimmywang1300@gmail.com>
2024-07-02 15:37:42 -0400

    delete base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/window-display/", 
   plugins: [react()],
   build: {
     outDir: 'dist'  // Ensure this matches the publish_dir in the workflow
